3a05b3265c2ff623db1bbbd18a83c27ed9c4afa6
fizzymagic <orders@fizzymagic.net>
2020-01-18 23:10:47 -0800

    updated tests; now using multiple tests per function.

--- test/SongTest.py
+++ test/SongTest.py
@@ -10,11 +10,13 @@ class SongTestCase(unittest.TestCase):
     def setUp(self):
         self.test_song = ctsSong.Song('twinkle.mid')
 
-    def test_total_notes(self):
+    def test_notes(self):
         """
         Tests that the total number of notes imported is correct.
         """
         self.assertEqual(self.test_song.stats['Notes'], 143)
+        self.assertEqual(max(n.note_num for t in self.test_song.tracks for n in t.notes), 69)
+        self.assertEqual(max(n.duration for t in self.test_song.tracks for n in t.notes), 3840)
 
     def test_tracks(self):
         """
@@ -26,23 +28,35 @@ class SongTestCase(unittest.TestCase):
         """
         Tests the quantization and polyphony functions of the Song class.
         """
+        self.assertFalse(self.test_song.is_quantized())
+        self.assertTrue(self.test_song.is_polyphonic())
+
         ts = copy.deepcopy(self.test_song)
         q_n, q_d = ts.estimate_quantization()
+        self.assertEqual(q_n, 480)
+        self.assertEqual(q_d, 480)
+
         ts.quantize(q_n, q_d)
         ts.remove_polyphony()
-
-        result = (not self.test_song.is_quantized() and self.test_song.is_polyphonic()
-                  and ts.is_quantized() and not ts.is_polyphonic()
-                  and ts.qticks_notes == 480 and ts.qticks_durations == 480)
-        self.assertTrue(result)
+        self.assertTrue(ts.is_quantized())
+        self.assertFalse(ts.is_polyphonic())
+        self.assertEqual(ts.qticks_notes, q_n)
+        self.assertEqual(ts.qticks_durations, q_d)
 
     def test_duration_to_note_name(self):
         """
         Test conversion of durations (in ticks) to note names
         """
         ppq = self.test_song.ppq
-        known_good = 'quarter eighth eighth triplet sixteenth thirty-second thirty-second triplet sixty-fourth'
+        known_good = 'quarter, eighth, eighth triplet, sixteenth, thirty-second, thirty-second triplet, sixty-fourth'
         test_durations = [1, 2, 3, 4, 8, 12, 16]
-        test_output = ' '.join(ctsSong.duration_to_note_name(ppq//n, ppq) for n in test_durations)
-
+        test_output = ', '.join(ctsSong.duration_to_note_name(ppq//n, ppq) for n in test_durations)
         self.assertEqual(test_output, known_good)
+
+    def test_measures(self):
+        """
+        Tests the measures handling
+        """
+        self.assertEqual(len(self.test_song.measure_beats), 52)
+        self.assertEqual(max(m.measure for m in self.test_song.measure_beats), 13)
+        self.assertEqual(max(m.beat for m in self.test_song.measure_beats), 4)
--- test/ctsQPlot.py
+++ test/ctsQPlot.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append('../src')
 import numpy as np
 import matplotlib.pyplot as plt
 import collections
@@ -18,7 +19,7 @@ def plot_quantization(stats, elements):
         x = []
         y = []
         last = 0.
-        for xv in range(max(s)):
+        for xv in range(min(s), max(s)):
             x.append(xv - 0.5)
             x.append(xv - 0.5)
             y.append(last)
@@ -28,11 +29,13 @@ def plot_quantization(stats, elements):
         x.append(xv + 0.5)
         y.append(last)
         y.append(0.)
+        y = [d + 0.1 for d in y]
         ax.plot(x, y, color=colors[i], label = e, zorder=2-i)
 
         ax.set_xlabel(e + ' (ticks)')
-        ax.set_xlim(0, max(x))
+        ax.set_xlim(min(min(x), 0.), max(x))
         ax.set_ylabel('Number')
+        ax.set_yscale('log')
         ax.set_ylim(0, max(y) * 1.1)
         ax.legend()
         ax.set_title('Statistics')
@@ -48,10 +51,10 @@ print('Estimated quantization:', q)
 
 in_song.quantize()
 
-in_song.eliminate_polyphony()
+in_song.remove_polyphony()
 
 print('\n'.join("%s: %s" % (s, in_song.stats[s]) for s in in_song.stats))
 
-plot_quantization(in_song.stats, ['Duration Deltas'])
+plot_quantization(in_song.stats, ['Note Start Deltas'])
 
 
