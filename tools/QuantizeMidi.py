7e213bbca5f25693c0e9aa75a410380c28d8935d
fizzymagic <orders@fizzymagic.net>
2020-02-05 10:33:14 -0800

    Added command-line options to QuantizeMidi.py

--- tools/QuantizeMidi.py
+++ tools/QuantizeMidi.py
@@ -1,41 +1,65 @@
 import sys
-
-sys.path.append('../src')
-
+import argparse
+from os import path
+import toolsPath
 import ctsChirp
+import ctsMidiImport
+import ctsMidiExport
 
-in_filename = sys.argv[1]
-out_filename = sys.argv[2]
 
-in_midi = ctsChirp.ChirpSong()
-in_midi.import_midi(in_filename)
+def main():
+    parser = argparse.ArgumentParser(description="Convert a midi file into a GoatTracker2 sng file.")
+    parser.add_argument('midi_in_file', help='midi filename to import')
+    parser.add_argument('midi_out_file', help='midi filename to export')
+    quant_group = parser.add_mutually_exclusive_group(required=False)
+    quant_group.add_argument('-q', '--quantizenote', type=str, help='quantize to a note value')
+    quant_group.add_argument('-t', '--quantizeticks', type=int, help='quantize to ticks')
 
-# Print stats
-print('%d notes' % (sum(len(t.notes) for t in in_midi.tracks)))
-print('PPQ = %d' % (in_midi.metadata.ppq))
-q_state = "" if in_midi.is_quantized() else "not"
-p_state = "" if in_midi.is_polyphonic() else "not"
-print("Input midi is %s quantized and %s polyphonic" % (q_state, p_state))
-qticks_n, qticks_d = in_midi.estimate_quantization()
-print("Estimated quantization = (%d, %d) ticks" % (qticks_n, qticks_d))
-print("                       = (%s, %s)" % (ctsChirp.duration_to_note_name(qticks_n, in_midi.metadata.ppq),
-                                             ctsChirp.duration_to_note_name(qticks_d, in_midi.metadata.ppq)))
+    args = parser.parse_args()
 
-print("Removing control notes...")
-in_midi.remove_control_notes()
+    if not (args.midi_in_file.lower().endswith(r'.mid') or args.midi_in_file.lower().endswith(r'.midi')):
+        parser.error(r'Expecting input filename that ends in ".mid"')
+    if not path.exists(args.midi_in_file):
+        parser.error('Cannot find "%s"' % args.midi_in_file)
 
-print("Quantizing...")
-in_midi.quantize()
+    song = ctsMidiImport.midi_to_chirp(args.midi_in_file)
 
-print("Eliminating polyphony...")
-in_midi.eliminate_polyphony()
-# inMidi.modulate(3, 2)
+    # Print stats
+    print('%d notes' % (sum(len(t.notes) for t in song.tracks)))
+    print('PPQ = %d' % (song.metadata.ppq))
+    q_state = "" if song.is_quantized() else "not"
+    p_state = "" if song.is_polyphonic() else "not"
+    print("Input midi is %s quantized and %s polyphonic" % (q_state, p_state))
 
-q_state = "" if in_midi.is_quantized() else "not"
-p_state = "" if in_midi.is_polyphonic() else "not"
-print("ChirpSong is %s quantized and %s polyphonic" % (q_state, p_state))
+    print("Removing control notes...")
+    song.remove_control_notes()
 
-print('\n'.join("%24s %s" % (s, str(v)) for s, v in in_midi.stats.items()))
+    print("Quantizing...")
+    if args.quantizenote:
+        print("to note value %s" % args.quantizenote)
+        song.quantize_from_note_name(args.quantizenote)
+    elif args.quantizeticks:
+        print('to %d ticks' % args.quantizeticks)
+        song.quantize(args.quantizeticks, args.quantizeticks)
+    else:
+        qticks_n, qticks_d = song.estimate_quantization()
+        print('to estimated quantization: %d, %d  ticks' % (qticks_n, qticks_d))
+        song.quantize(qticks_n, qticks_d)
 
-print("Exporting to MIDI...")
-in_midi.export_midi(out_filename)
+
+    print("Eliminating polyphony...")
+    song.remove_polyphony()
+    # inMidi.modulate(3, 2)
+
+    q_state = "" if song.is_quantized() else "not"
+    p_state = "" if song.is_polyphonic() else "not"
+    print("ChirpSong is %s quantized and %s polyphonic" % (q_state, p_state))
+
+    print('\n'.join("%24s %s" % (s, str(v)) for s, v in song.stats.items()))
+
+    print("Exporting to MIDI...")
+    ctsMidiExport.chirp_to_midi(song, args.midi_out_file)
+
+
+if __name__ == '__main__':
+    main()
