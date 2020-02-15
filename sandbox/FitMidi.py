81ee30eeb08e2d65da61429085bd220e3058a89f
fizzymagic <orders@fizzymagic.net>
2020-02-14 20:54:22 -0800

    Improved the quantizer for songs.  It is now much more stable.

--- sandbox/FitMidi.py
+++ sandbox/FitMidi.py
@@ -6,39 +6,58 @@ import ctsMidi
 song = ctsMidi.midi_to_chirp(sys.argv[1])
 
 desired_ppq = 960
-desired_q = 960 // 4
-factor = 1.0
-
+desired_q = 960 // 4  # Sixteenth notes are the target here.
 
 notes = [n for t in song.tracks for n in t.notes]
 
-def objective_function(params):
-    offset, scale_factor = params
+def objective_function(offset, scale_factor):
     err = 0
     for n in notes:
         start = (n.start_time - offset) * scale_factor
         delta = quantization_error(start, desired_q)
-        err += delta * delta / scale_factor
+        err += abs(delta)
     return err
 
-
-fstart = 960 / song.metadata.ppq / 2
-fend = fstart * 2
-f = fstart
-offset_est = min(n.start_time for n in notes)
-best_f = f
-best_offset = offset_est
-min_e = 1.e7
-while f < fend:
-    for offset in range(offset_est - 10, offset_est + 10):
-        e = objective_function([offset, f])
+def find_best_f(f_start, f_end, step, offset):
+    min_e = objective_function(offset, f_start)
+    best_f = f_start
+    n_steps = int((f_end - f_start) // step) + 1
+    for i in range(n_steps):
+        f = f_start + i * step
+        e = objective_function(offset, f)
         if e < min_e:
             min_e = e
             best_f = f
-            best_offset = offset
-        f += 0.001
+    return (best_f, min_e)
 
-print(best_f, best_offset, min_e)
+def find_best_offset(o_start, o_end, f):
+    min_e = objective_function(o_start, f)
+    best_offset = o_start
+    n_steps = (o_end - o_start)
+    for offset in range(o_start, o_end + 1):
+        e = objective_function(offset, f)
+        if e < min_e:
+            min_e = e
+            best_offset = offset
+    return (best_offset, min_e)
+
+f_min = round(960 / song.metadata.ppq / 2, 3)
+f_max = f_min * 2
+f_step = .01
+offset_est = min(n.start_time for n in notes)
+last_min_e = 1.e9
+best_f, min_e = find_best_f(f_min, f_max, f_step, offset_est)
+best_offset, min_e = find_best_offset(offset_est - 20, offset_est + 20, best_f)
+
+while min_e < last_min_e:
+    last_min_e = min_e
+    f_step /= 10.
+    f_min = best_f - (f_step * 100)
+    f_max = best_f + (f_step * 100)
+    best_f, min_e = find_best_f(f_min, f_max, f_step, best_offset)
+    best_offset, min_e = find_best_offset(offset_est - 20, offset_est + 20, best_f)
+
+print("%.5lf, %d, %.6lf" % (best_f, best_offset, min_e))
 
 song.move_ticks(-best_offset)
 song.scale_ticks(best_f)
