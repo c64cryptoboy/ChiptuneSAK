3c121dda635772e0245c4d829f67d9a3e0fd74a9
David Youd <cryptoboy@gmail.com>
2020-02-27 10:43:24 -0800

    added midiDump.py

---
+++ sandbox/midiDump.py
@@ -0,0 +1,22 @@
+# Placeholder for something better
+
+import sys
+from mido import MidiFile
+
+if len(sys.argv) != 2:
+    print ("Usage: midiFilter filename.midi")
+    exit()
+
+inFilename = sys.argv[1]
+try:
+    inMidiFile = MidiFile(inFilename)
+except IOError:
+    print('Error: Can\'t open "%s"' % (inFilename))
+    sys.exit(1)
+		
+print ("Midi type: %d, PPQ: %d\n" % (inMidiFile.type, inMidiFile.ticks_per_beat))
+for i, track in enumerate(inMidiFile.tracks):
+    print('Track {}: "{}"'.format(i, track.name))
+    for msg in track:
+        print(msg)
+
