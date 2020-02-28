e33378f2b1b621183a395da0ee215e96c30a2001
David Youd <cryptoboy@gmail.com>
2020-02-27 19:10:11 -0800

    gtTest.py now does a good test

--- test/gtTest.py
+++ test/gtTest.py
@@ -1,15 +1,13 @@
-# test goat tracker functionality (including parsing and conversion to chirp)
+# Test goat tracker functionality (including parsing and conversion to chirp)
 #
 # TODOs:
 # - Add an additional subtune to gtTestData.sng and create tests here for it
-
 
 import testingPath
 import unittest
 import ctsGoatTracker
 import ctsTestingTools
-import ctsBase
-
+from  ctsBase import note_name_to_pitch
 
 class TestGoatTrackerFunctions(unittest.TestCase):
     def test_sng_parsing(self):
@@ -21,6 +19,29 @@ class TestGoatTrackerFunctions(unittest.TestCase):
         sng_data = ctsGoatTracker.import_sng(sng_in_file)
         # get channel time events for subtune 0
         channels_time_events = ctsGoatTracker.convert_to_note_events(sng_data, 0)
+
+        # TODO: Write parsing tests on channels_time_events
+
+        """
+        # Generate ground truth data for this test (comment out later, but keep it around)
+        print("channels = []")
+        for i, channel in enumerate(channels_time_events):
+            channel_note_on_events = []
+            for frame, event in channel.items():
+                if event.note_on:
+                    midi_note_name = ctsBase.pitch_to_note_name(event.note)
+                    channel_note_on_events.append('(%d, "%s")' % (frame, midi_note_name))
+            line = "channels.append((%s))" % (', '.join(channel_note_on_events))
+            print(line)
+        """
+
+        channels = []
+        channels.append(((0, "G4"), (72, "C5"), (144, "E5"), (216, "G#4"), (288, "C#5"), (360, "F5"), (432, "G#4"), \
+            (504, "C#5"), (576, "F5"), (648, "G4"), (720, "C5"), (792, "E5")))
+        channels.append(((0, "E4"), (72, "G4"), (144, "C5"), (216, "F4"), (288, "G#4"), (360, "C#5"), (432, "F4"), \
+            (504, "G#4"), (576, "C#5"), (648, "E4"), (720, "G4"), (792, "C5")))
+        channels.append(((0, "C4"), (72, "E4"), (144, "G4"), (216, "C#4"), (288, "F4"), (360, "G#4"), (432, "C#4"), \
+            (504, "F4"), (576, "G#4"), (648, "C4"), (720, "E4"), (792, "G4")))        
 
         """
         Description of gtTestData.sng subtune 0 playback
@@ -39,29 +60,13 @@ class TestGoatTrackerFunctions(unittest.TestCase):
         This supports testing of related tempos across channels.
         """
 
-        # TODO: Write parsing tests on channels_time_events
-
-        """
-        # Generate ground truth data for this test (comment out later, but keep it around)
-        print("channels = []")
-        for i, channel in enumerate(channels_time_events):
-            channel_note_on_events = []
-            for frame, event in channel.items():
-                if event.note_on:
-                    midi_note_name = ctsBase.pitch_to_note_name(event.note)
-                    channel_note_on_events.append('(%d, "%s")' % (frame, midi_note_name))
-            line = "channels.append((%s))" % (', '.join(channel_note_on_events))
-            print(line)
-        """
-
-    channels = []
-    channels.append(((0, "G4"), (72, "C5"), (144, "E5"), (216, "G#4"), (288, "C#5"), (360, "F5"), (432, "G#4"), \
-        (504, "C#5"), (576, "F5"), (648, "G4"), (720, "C5"), (792, "E5")))
-    channels.append(((0, "E4"), (72, "G4"), (144, "C5"), (216, "F4"), (288, "G#4"), (360, "C#5"), (432, "F4"), \
-        (504, "G#4"), (576, "C#5"), (648, "E4"), (720, "G4"), (792, "C5")))
-    channels.append(((0, "C4"), (72, "E4"), (144, "G4"), (216, "C#4"), (288, "F4"), (360, "G#4"), (432, "C#4"), \
-        (504, "F4"), (576, "G#4"), (648, "C4"), (720, "E4"), (792, "G4")))        
+        for i, channel in enumerate(channels):
+            for tup in channel:
+                # do enharmonic note comparisons
+                parsed_notes = channels_time_events[i][tup[0]].note
+                asserted_note = note_name_to_pitch(tup[1])                
+                self.assertEqual(parsed_notes, asserted_note)
 
 if __name__ == '__main__':
     #ctsTestingTools.env_to_stdout()
-    unittest.main()
+    unittest.main(failfast=True) # Lots of asserts (in a loop), so stop after first fail
