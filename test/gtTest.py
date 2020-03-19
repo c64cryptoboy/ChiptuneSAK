cd6fbdbf7b142c7945f6ca0783debed60dc5f2cb
David Youd <cryptoboy@gmail.com>
2020-03-19 14:23:22 -0700

    Make gtTest.py work with rchirp changes

--- test/gtTest.py
+++ test/gtTest.py
@@ -7,66 +7,66 @@ import testingPath
 import unittest
 import ctsGoatTracker
 import ctsTestingTools
-from  ctsBase import note_name_to_pitch
+import ctsBase
 
 class TestGoatTrackerFunctions(unittest.TestCase):
-    @unittest.skip("GT import testing not working now...")
+    
+    #@unittest.skip("GT import testing not working now...")
+    
     def test_sng_parsing(self):
         # TODO: I'm fighting environment again...
+        #sng_in_file = 'test/data/gtTestData.sng'
         sng_in_file = 'data/gtTestData.sng'
-        #sng_in_file = 'data/gtTestData.sng'
 
         # parse all subtunes
-        sng_data = ctsGoatTracker.import_sng(sng_in_file)
-        # get channel time events for subtune 0
-        channels_time_events = ctsGoatTracker.convert_to_note_events(sng_data, 0)
-
-        # TODO: Write parsing tests on channels_time_events
+        parsed_gt = ctsGoatTracker.import_sng(sng_in_file)
+        # get rchirp for subtune 0
+        rchirp_song = ctsGoatTracker.convert_parsed_gt_to_rchirp(parsed_gt, 0)
 
         """
-        # Generate ground truth data for this test (comment out later, but keep it around)
-        print("channels = []")
-        for i, channel in enumerate(channels_time_events):
+        # Generate expected output data for this test (comment out later, but keep it around)
+        print("expected_channels = []")
+        for channel in rchirp_song.voices:
+            channel = channel.rows
             channel_note_on_events = []
-            for frame, event in channel.items():
-                if event.note_on:
-                    midi_note_name = ctsBase.pitch_to_note_name(event.note)
-                    channel_note_on_events.append('(%d, "%s")' % (frame, midi_note_name))
-            line = "channels.append((%s))" % (', '.join(channel_note_on_events))
+            for rchirp_row in channel.values():
+                if rchirp_row.gate:
+                    midi_note_name = ctsBase.pitch_to_note_name(rchirp_row.note_num)
+                    channel_note_on_events.append('(%d, "%s")' % (rchirp_row.jiffy_num, midi_note_name))
+            line = "expected_channels.append((%s))" % (', '.join(channel_note_on_events))
             print(line)
         """
+        
+        expected_channels = []
+        expected_channels.append(((0, "G4"), (72, "C5"), (144, "E5"), (216, "G#4"), (288, "C#5"), (360, "F5"), \
+            (432, "G#4"), (504, "C#5"), (576, "F5"), (648, "G4"), (720, "C5"), (792, "E5")))
+        expected_channels.append(((0, "E4"), (72, "G4"), (144, "C5"), (216, "F4"), (288, "G#4"), (360, "C#5"), \
+            (432, "F4"), (504, "G#4"), (576, "C#5"), (648, "E4"), (720, "G4"), (792, "C5")))
+        expected_channels.append(((0, "C4"), (72, "E4"), (144, "G4"), (216, "C#4"), (288, "F4"), (360, "G#4"), \
+            (432, "C#4"), (504, "F4"), (576, "G#4"), (648, "C4"), (720, "E4"), (792, "G4")))        
 
-        channels = []
-        channels.append(((0, "G4"), (72, "C5"), (144, "E5"), (216, "G#4"), (288, "C#5"), (360, "F5"), (432, "G#4"), \
-            (504, "C#5"), (576, "F5"), (648, "G4"), (720, "C5"), (792, "E5")))
-        channels.append(((0, "E4"), (72, "G4"), (144, "C5"), (216, "F4"), (288, "G#4"), (360, "C#5"), (432, "F4"), \
-            (504, "G#4"), (576, "C#5"), (648, "E4"), (720, "G4"), (792, "C5")))
-        channels.append(((0, "C4"), (72, "E4"), (144, "G4"), (216, "C#4"), (288, "F4"), (360, "G#4"), (432, "C#4"), \
-            (504, "F4"), (576, "G#4"), (648, "C4"), (720, "E4"), (792, "G4")))        
+        # Description of gtTestData.sng subtune 0 playback
+        # 
+        # Patterns 0, 1, and 2 play simultaneously, giving a C major chord, then its 1st and 2nd inversions.
+        # This is for simple testing.
+        #
+        # Then patterns 0, 1, 2 are transposed up a half step, and this transposed playback is done twice
+        # (R2 command).  The transpose is then removed.
+        # This supports testing of repeats and transpositions.
+        #
+        # Finally, patterns 7, 8, and 9 play simultaneously, giving a C major chord, then its 1st and 2nd
+        # inversions.  However, channel one is running at tempo 24 (3 rows per note), channel two at tempo
+        # 12 (6 rows per note), and channel three at tempo 6 (12 rows per note).  Tempo * rows-per-note is
+        # constant across all three channels, so the channels play back with (chord) simultaneity.
+        # This supports testing of related tempos across channels.
 
-        """
-        Description of gtTestData.sng subtune 0 playback
-
-        Patterns 0, 1, and 2 play simultaneously, giving a C major chord, then its 1st and 2nd inversions.
-        This is for simple testing.
-
-        Then patterns 0, 1, 2 are transposed up a half step, and this transposed playback is done twice
-        (R2 command).  The transpose is then removed.
-        This supports testing of repeats and transpositions.
-
-        Finally, patterns 7, 8, and 9 play simultaneously, giving a C major chord, then its 1st and 2nd
-        inversions.  However, channel one is running at tempo 24 (3 rows per note), channel two at tempo
-        12 (6 rows per note), and channel three at tempo 6 (12 rows per note).  Tempo * rows-per-note is
-        constant across all three channels, so the channels play back with (chord) simultaneity.
-        This supports testing of related tempos across channels.
-        """
-
-        for i, channel in enumerate(channels):
-            for tup in channel:
-                # do enharmonic note comparisons
-                parsed_notes = channels_time_events[i][tup[0]].note
-                asserted_note = note_name_to_pitch(tup[1])                
-                self.assertEqual(parsed_notes, asserted_note)
+        for i, expected_channel in enumerate(expected_channels):
+            actual_channel = rchirp_song.voices[i].get_jiffy_indexed_rows()
+            for expected_jiffy, expected_note in expected_channel:
+                rchirp_row = actual_channel[expected_jiffy]
+                self.assertIsNotNone(rchirp_row) # Make sure there's an actual entry at that expected jiffy
+                actual_note = ctsBase.pitch_to_note_name(rchirp_row.note_num) # do enharmonic comparison              
+                self.assertEqual(actual_note, expected_note)
 
 if __name__ == '__main__':
     #ctsTestingTools.env_to_stdout()
