3a438e00f3a825716a1c7a063ffac14e39fd72f3
David Youd <cryptoboy@gmail.com>
2020-05-16 18:28:45 -0700

    WIP on deep emulation testing

---
+++ test/wolfgangLorenz6510Tests/tests.py
@@ -0,0 +1,363 @@
+# This program runs Wolfgang Lorenz' C64 test suite (2002, public domain)
+# This standard set of tests for C64 emulators can take a LONG time to run
+#
+# to run: python -m unittest -v tests
+#
+# TODO:
+# - write code to load each test binary and run it.
+# - will need to stub out:
+# -- $E16F (so python unit tests can do the loading instead)
+# -- $FFE4 (so that no there's no user keyboard interaction)
+# -- $FFD2 (so I can tell status without looking at screen memory)
+
+from parameterized import parameterized, parameterized_class
+import unittest
+
+binary_file_tests = [
+    ("adca",  "adc absolute"),
+    ("adcax", "adc absolute,x"),
+    ("adcay", "adc absolute,y"),
+    ("adcb", "adc immediate"),
+    ("adcix", "adc (indirect,x)"),
+    ("adciy", "adc (indirect),y"),
+    ("adcz", "adc zeropage"),
+    ("adczx", "adc zeropage,x"),
+    ("alrb", "alr immediate"),
+    ("ancb", "anc immediate"),
+    ("anda", "and absolute"),
+    ("andax", "and absolute,x"),
+    ("anday", "and absolute,y"),
+    ("andb", "and immediate"),
+    ("andix", "and (indirect,x)"),
+    ("andiy", "and (indirect),y"),
+    ("andz", "and zeropage"),
+    ("andzx", "and zeropage,x"),
+    ("aneb", "ane immediate"),
+    ("arrb", "arr immediate"),
+    ("asla", "asl absolute"),
+    ("aslax", "asl absolute,x"),
+    ("asln", "asl"),
+    ("aslz", "asl zeropage"),
+    ("aslzx", "asl zeropage,x"),
+    ("asoa", "aso absolute"),
+    ("asoax", "aso absolute,x"),
+    ("asoay", "aso absolute,y"),
+    ("asoix", "aso (indirect,x)"),
+    ("asoiy", "aso (indirect),y"),
+    ("asoz", "aso zeropage"),
+    ("asozx", "aso zeropage,x"),
+    ("axsa", "axs absolute"),
+    ("axsix", "axs (indirect,x)"),
+    ("axsz", "axs zeropage"),
+    ("axszy", "axs zeropage,y"),
+    ("bccr", "bcc relative"),
+    ("bcsr", "bcs relative"),
+    ("beqr", "beq relative"),
+    ("bita", "bit absolute"),
+    ("bitz",	"bit zeropage"),
+    ("bmir", "bmi relative"),
+    ("bner", "bne relative"),
+    ("bplr", "bpl relative"),
+    ("branchwrap", "branchwrap"),
+    ("brkn", "brk"),
+    ("bvcr", "bvc relative"),
+    ("bvsr", "bvs relative"),
+    ("cia1pb6", "cia1pb6"),
+    ("cia1pb7", "cia1pb7"),
+    ("cia1ta", "cia1ta"),
+    ("cia1tab", "cia1tab"),
+    ("cia1tb", "cia1tb"),
+    ("cia1tb123", "cia1tb123"),
+    ("cia2pb6", "cia2pb6"),
+    ("cia2pb7", "cia2pb7"),
+    ("cia2ta", "cia2ta"),
+    ("cia2tb", "cia2tb"),
+    ("cia2tb123", "cia2tb123"),
+    ("clcn", "clc"),
+    ("cldn", "cld"),
+    ("clin", "cli"),
+    ("clvn", "clv"),
+    ("cmpa", "cmp absolute"),
+    ("cmpax", "cmp absolute,x"),
+    ("cmpay", "cmp absolute,y"),
+    ("cmpb", "cmp immediate"),
+    ("cmpix", "cmp (indirect,x)"),
+    ("cmpiy", "cmp (indirect),y"),
+    ("cmpz", "cmp zeropage"),
+    ("cmpzx", "cmp zeropage,x"),
+    ("cntdef", "cntdef"),
+    ("cnto2", "cnto2"),
+    ("cpuport", "cpuport"),
+    ("cputiming", "cputiming"),
+    ("cpxa", "cpx absolute"),
+    ("cpxb", "cpx immediate"),
+    ("cpxz", "cpx zeropage"),
+    ("cpya", "cpy absolute"),
+    ("cpyb", "cpy immediate"),
+    ("cpyz", "cpy zeropage"),
+    ("dcma", "dcm absolute"),
+    ("dcmax", "dcm absolute,x"),
+    ("dcmay", "dcm absolute,y"),
+    ("dcmix", "dcm (indirect,x)"),
+    ("dcmiy", "dcm (indirect),y"),
+    ("dcmz", "dcm zeropage"),
+    ("dcmzx", "dcm zeropage,x"),
+    ("deca", "dec absolute"),
+    ("decax", "dec absolute,x"),
+    ("decz", "dec zeropage"),
+    ("deczx", "dec zeropage,x"),
+    ("dexn", "dex"),
+    ("deyn", "dey"),
+    ("eora", "eor absolute"),
+    ("eorax", "eor absolute,x"),
+    ("eoray", "eor absolute,y"),
+    ("eorb", "eor immediate"),
+    ("eorix", "eor (indirect,x)"),
+    ("eoriy", "eor (indirect),y"),
+    ("eorz", "eor zeropage"),
+    ("eorzx", "eor zeropage,x"),
+    ("flipos", "flipos"),
+    ("icr01", "icr01"),
+    ("imr", "imr                   TODO ?????"),
+    ("inca", "inc absolute"),
+    ("incax", "inc absolute,x"),
+    ("incz", "inc zeropage"),
+    ("inczx", "inc zeropage,x"),
+    ("insa", "ins absolute"),
+    ("insax", "ins absolute,x"),
+    ("insay", "ins absolute,y"),
+    ("insix", "ins (indirect,x)"),
+    ("insiy", "ins (indirect),y"),
+    ("insz", "ins zeropage"),
+    ("inszx", "ins zeropage,x"),
+    ("inxn", "inx"),
+    ("inyn", "iny"),
+    ("irq", "irq"),
+    ("jmpi", "jmp indirect"),
+    ("jmpw", "jmp absolute"),
+    ("jsrw", "jsr absolute"),
+    ("lasay", "las absolute,y"),
+    ("laxa", "lax absolute"),
+    ("laxay", "lax absolute,y"),
+    ("laxix", "lax (indirect,x)"),
+    ("laxiy", "lax (indirect),y"),
+    ("laxz", "lax zeropage"),
+    ("laxzy", "lax zeropage,y"),
+    ("ldaa", "lda absolute"),
+    ("ldaax", "lda absolute,x"),
+    ("ldaay", "lda absolute,y"),
+    ("ldab", "lda immediate"),
+    ("ldaix", "lda (indirect,x)"),
+    ("ldaiy", "lda (indirect),y"),
+    ("ldaz", "lda zeropage"),
+    ("ldazx", "lda zeropage,x"),
+    ("ldxa", "ldx absolute"),
+    ("ldxay", "ldx absolute,y"),
+    ("ldxb", "ldx immediate"),
+    ("ldxz", "ldx zeropage"),
+    ("ldxzy", "ldx zeropage,y"),
+    ("ldya", "ldy absolute"),
+    ("ldyax", "ldy absolute,x"),
+    ("ldyb", "ldy immediate"),
+    ("ldyz", "ldy zeropage"),
+    ("ldyzx", "ldy zeropage,x"),
+    ("loadth", "loadth"),
+    ("lsea", "lse absolute"),
+    ("lseax", "lse absolute,x"),
+    ("lseay", "lse absolute,y"),
+    ("lseix", "lse (indirect,x)"),
+    ("lseiy", "lse (indirect),y"),
+    ("lsez", "lse zeropage"),
+    ("lsezx", "lse zeropage,x"),
+    ("lsra", "lsr absolute"),
+    ("lsrax", "lsr absolute,x"),
+    ("lsrn", "lsr"),
+    ("lsrz", "lsr zeropage"),
+    ("lsrzx", "lsr zeropage,x"),
+    ("lxab", "lxa immediate"),
+    ("mmu", "mmu"),
+    ("mmufetch", "mmufetch"),
+    ("nmi", "nmi"),
+    ("nopa", "nop absolute"),
+    ("nopax", "nop absolute,x"),
+    ("nopb", "nop immediate"),
+    ("nopn", "nop"),
+    ("nopz", "nop zeropage"),
+    ("nopzx", "nop zeropage,x"),
+    ("oneshot", "oneshot"),
+    ("oraa", "ora absolute"),
+    ("oraax", "ora absolute,x"),
+    ("oraay", "ora absolute,y"),
+    ("orab", "ora immediate"),
+    ("oraix", "ora (indirect,x)"),
+    ("oraiy", "ora (indirect),y"),
+    ("oraz", "ora zeropage"),
+    ("orazx", "ora zeropage,x"),
+    ("phan", "pha"),
+    ("phpn", "php"),
+    ("plan", "pla"),
+    ("plpn", "plp"),
+    ("rlaa", "rla absolute"),
+    ("rlaax", "rla absolute,x"),
+    ("rlaay", "rla absolute,y"),
+    ("rlaix", "rla (indirect,x)"),
+    ("rlaiy", "rla (indirect),y"),
+    ("rlaz", "rla zeropage"),
+    ("rlazx", "rla zeropage,x"),
+    ("rola", "rol absolute"),
+    ("rolax", "rol absolute,x"),
+    ("roln", "rol"),
+    ("rolz", "rol zeropage"),
+    ("rolzx", "rol zeropage,x"),
+    ("rora", "ror absolute"),
+    ("rorax", "ror absolute,x"),
+    ("rorn", "ror"),
+    ("rorz", "ror zeropage"),
+    ("rorzx", "ror zeropage,x"),
+    ("rraa", "rra absolute"),
+    ("rraax", "rra absolute,x"),
+    ("rraay", "rra absolute,y"),
+    ("rraix", "rra (indirect,x)"),
+    ("rraiy", "rra (indirect),y"),
+    ("rraz", "rra zeropage"),
+    ("rrazx", "rra zeropage,x"),
+    ("rtin", "rti"),
+    ("rtsn", "rts"),
+    ("sbca", "sbc absolute"),
+    ("sbcax", "sbc absolute,x"),
+    ("sbcay", "sbc absolute,y"),
+    ("sbcb", "sbc immediate"),
+    ("sbcb(eb)", "sbcb(eb)"),
+    ("sbcix", "sbc (indirect,x)"),
+    ("sbciy", "sbc (indirect),y"),
+    ("sbcz", "sbc zeropage"),
+    ("sbczx", "sbc zeropage,x"),
+    ("sbxb", "sbx immediate"),
+    ("secn", "sec"),
+    ("sedn", "sed"),
+    ("sein", "sei"),
+    ("shaay", "sha absolute,y"),
+    ("shaiy", "sha (indirect),y"),
+    ("shsay", "shs absolute,y"),
+    ("shxay", "shx absolute,y"),
+    ("shyax", "shy absolute,x"),
+    ("staa", "sta absolute"),
+    ("staax", "sta absolute,x"),
+    ("staay", "sta absolute,y"),
+    ("staix", "sta (indirect,x)"),
+    ("staiy", "sta (indirect),y"),
+    ("staz", "sta zeropage"),
+    ("stazx", "sta zeropage,x"),
+    ("stxa", "stx absolute"),
+    ("stxz", "stx zeropage"),
+    ("stxzy", "stx zeropage,y"),
+    ("stya", "sty absolute"),
+    ("styz", "sty zeropage"),
+    ("styzx", "sty zeropage,x"),
+    ("taxn", "tax"),
+    ("tayn", "tay"),
+    ("trap1", "trap1"),
+    ("trap2", "trap2"),
+    ("trap3", "trap3"),
+    ("trap4", "trap4"),
+    ("trap5", "trap5"),
+    ("trap6", "trap6"),
+    ("trap7", "trap7"),
+    ("trap8", "trap8"),
+    ("trap9", "trap9"),
+    ("trap10", "trap10"),
+    ("trap11", "trap11"),
+    ("trap12", "trap12"),
+    ("trap13", "trap13"),
+    ("trap14", "trap14"),
+    ("trap15", "trap15"),
+    ("trap16", "trap16"),
+    ("trap17", "trap17"),
+    ("tsxn", "tsx"),
+    ("txan", "txa"),
+    ("txsn", "txs"),
+    ("tyan", "tya")]
+
+
+"""
+Hints on how to stub out BASIC/KERNAL calls to run the tests in a light emulation:
+http://www.softwolves.com/arkiv/cbm-hackers/7/7114.html
+
+    - Check the filename for "trap17" and exit if it is, since this is 
+where testing of non-6510 stuff begins
+
+     - Read the starting address from the two first bytes of the file 
+(LO, HI)
+
+     - Load the rest of the file into memory at the specified starting 
+address
+
+     - Initialize some memory locations:
+
+         $0002 = $00
+         $A002 = $00
+         $A003 = $80
+         $FFFE = $48
+         $FFFF = $FF
+         $01FE = $FF
+         $01FF = $7F
+
+     - Set up the KERNAL "IRQ handler" at $FF48:
+
+         FF48  48        PHA
+         FF49  8A        TXA
+         FF4A  48        PHA
+         FF4B  98        TYA
+         FF4C  48        PHA
+         FF4D  BA        TSX
+         FF4E  BD 04 01  LDA    $0104,X
+         FF51  29 10     AND    #$10
+         FF53  F0 03     BEQ    $FF58
+         FF55  6C 16 03  JMP    ($0316)
+         FF58  6C 14 03  JMP    ($0314)
+
+     - Put trap instructions at $FFD2, $E16F, $FFE4, $8000 and $A474
+
+     - Set S to $FD, P to $04 and PC to $0801
+
+The trap handler does the following:
+
+if PC == $FFD2 (Print character):
+     Set $030C = 0
+     Print PETSCII character corresponding to value of A
+     Pop return address from stack
+     Set PC to return address
+     Re-start the CPU
+
+if PC == $E16F (Load):
+     $BB is PETSCII filename address, low byte
+     $BC is PETSCII filename address, high byte
+     $B7 is PETSCII filename lenght
+     Load the file
+     Pop return address from stack
+     Set PC to $0816
+     Re-start the CPU
+
+if PC == $FFE4 (Scan keyboard):
+     Set A to 3
+     Pop return address from stack
+     Set PC to return address
+     Re-start the CPU
+
+if PC == $8000:
+if PC == $A474:
+     Exit
+"""
+
+class TestWolfgangLorenzPrograms(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @parameterized.expand(binary_file_tests)
+    def test_wl(self, file_name, test_name):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    # ctsTestingTools.env_to_stdout()
+    unittest.main(failfast=False)
