c7c8fc27f3cd187edf93f1b3984c74019a3a6b1a
David Youd <cryptoboy@gmail.com>
2020-05-19 15:53:26 -0700

    159 tests work now

--- test/wolfgangLorenz6510Tests/tests.py
+++ test/wolfgangLorenz6510Tests/tests.py
@@ -97,12 +97,17 @@ binary_file_tests = [
     #     
     # ("arrb", "arr immediate"),
 
-    # ASL tests
+    # test shifting (ASL, LSR)
     ("asla", "asl absolute"),
     ("aslax", "asl absolute,x"),
     ("asln", "asl"),
     ("aslz", "asl zeropage"),
     ("aslzx", "asl zeropage,x"),
+    ("lsra", "lsr absolute"),
+    ("lsrax", "lsr absolute,x"),
+    ("lsrn", "lsr"),
+    ("lsrz", "lsr zeropage"),
+    ("lsrzx", "lsr zeropage,x"),
 
     # illegal op  not yet supported in our emulator
     # 
@@ -190,6 +195,10 @@ binary_file_tests = [
     #("flipos", "flipos"),
     #("icr01", "icr01"),
     #("imr", "imr"),
+    #("irq", "irq"),
+    #("loadth", "loadth"),
+    #("nmi", "nmi"),
+    #("oneshot", "oneshot"),
 
     # clear flag instruction tests
     ("clcn", "clc"),
@@ -215,10 +224,12 @@ binary_file_tests = [
     ("cpyb", "cpy immediate"),
     ("cpyz", "cpy zeropage"),
 
-    # Test 6510 ports at mem loc 0 and 1, bits 0-7
+    # Test 6510 ports at mem loc 0 and 1
     # Skipping this for now
     #
-    # ("cpuport", "cpuport"),
+    #("cpuport", "cpuport test, bits 0-7"),
+    #("mmu", "mmu test, bits 0-2"), 
+    #("mmufetch", "mmufetch"),    
 
     # Test various cycles consumed.  If there's a problem, you'll see this:
     # xx command byte
@@ -348,17 +359,48 @@ binary_file_tests = [
     #("insz", "ins zeropage"),
     #("inszx", "ins zeropage,x"),
 
-    ("irq", "irq"),
+    # jump tests (JMP, JSR)
     ("jmpi", "jmp indirect"),
     ("jmpw", "jmp absolute"),
     ("jsrw", "jsr absolute"),
-    ("lasay", "las absolute,y"),
+
+    # illegal op, most of which has been implemented in our emulator
+    # 
+    # LAS    ***
+    # This opcode ANDs the contents of a memory location with the contents of the 
+    # stack pointer register and stores the result in the accumulator, the X 
+    # register, and the stack pointer.  Affected flags: N Z.
+    # One supported mode:
+    # LAS abcd,Y      ;BB cd ab    ;No. Cycles= 4*
+    #     
+    #("lasay", "las absolute,y"),
+
+    # illegal op not yet supported in our emulator
+    # 
+    # LAX    ***
+    # This opcode loads both the accumulator and the X register with the contents 
+    # of a memory location.
+    # Supported modes:
+    # LAX abcd        ;AF cd ab    ;No. Cycles= 4
+    # LAX abcd,Y      ;BF cd ab    ;            4*
+    # LAX ab          ;A7 ab       ;*=add 1     3
+    # LAX ab,Y        ;B7 ab       ;if page     4
+    # LAX (ab,X)      ;A3 ab       ;boundary    6
+    # LAX (ab),Y      ;B3 ab       ;is crossed  5*
+    # (Sub-instructions: LDA, LDX)
+    # Example:
+    # LAX $8400,Y     ;BF 00 84
+    # Equivalent instructions:
+    # LDA $8400,Y
+    # LDX $8400,Y
     ("laxa", "lax absolute"),
-    ("laxay", "lax absolute,y"),
+    #("laxay", "lax absolute,y"), # not implemented yet
     ("laxix", "lax (indirect,x)"),
     ("laxiy", "lax (indirect),y"),
     ("laxz", "lax zeropage"),
     ("laxzy", "lax zeropage,y"),
+
+    # load tests (LDA, LDX, LDY)
     ("ldaa", "lda absolute"),
     ("ldaax", "lda absolute,x"),
     ("ldaay", "lda absolute,y"),
@@ -377,7 +419,26 @@ binary_file_tests = [
     ("ldyb", "ldy immediate"),
     ("ldyz", "ldy zeropage"),
     ("ldyzx", "ldy zeropage,x"),
-    ("loadth", "loadth"),
+
+    # illegal op not yet supported in our emulator
+    # 
+    # LSE    ***   (aka SRE)
+    # LSE LSRs the contents of a memory location and then EORs the result with 
+    # the accumulator.
+    # Supported modes:
+    # LSE abcd        ;4F cd ab    ;No. Cycles= 6
+    # LSE abcd,X      ;5F cd ab    ;            7
+    # LSE abcd,Y      ;5B cd ab    ;            7
+    # LSE ab          ;47 ab       ;            5
+    # LSE ab,X        ;57 ab       ;            6
+    # LSE (ab,X)      ;43 ab       ;            8
+    # LSE (ab),Y      ;53 ab       ;            8
+    # (Sub-instructions: EOR, LSR)
+    # Example:
+    # LSE $C100,X     ;5F 00 C1
+    # Here's the same code using equivalent instructions.
+    # LSR $C100,X
+    # EOR $C100,X
     ("lsea", "lse absolute"),
     ("lseax", "lse absolute,x"),
     ("lseay", "lse absolute,y"),
@@ -385,22 +446,45 @@ binary_file_tests = [
     ("lseiy", "lse (indirect),y"),
     ("lsez", "lse zeropage"),
     ("lsezx", "lse zeropage,x"),
-    ("lsra", "lsr absolute"),
-    ("lsrax", "lsr absolute,x"),
-    ("lsrn", "lsr"),
-    ("lsrz", "lsr zeropage"),
-    ("lsrzx", "lsr zeropage,x"),
-    ("lxab", "lxa immediate"),
-    ("mmu", "mmu"),
-    ("mmufetch", "mmufetch"),
-    ("nmi", "nmi"),
-    ("nopa", "nop absolute"),
-    ("nopax", "nop absolute,x"),
-    ("nopb", "nop immediate"),
-    ("nopn", "nop"),
-    ("nopz", "nop zeropage"),
-    ("nopzx", "nop zeropage,x"),
-    ("oneshot", "oneshot"),
+
+    # OAL    *** (aka LXA)
+    # This opcode ORs the A register with #$EE, ANDs the result with an immediate 
+    # value, and then stores the result in both A and X.
+    # One supported mode:
+    # OAL #ab         ;AB ab       ;No. Cycles= 2
+    # Here's an example of how you might use this opcode:
+    # OAL #$AA        ;AB AA
+    # Here's the same code using equivalent instructions:
+    # ORA #$EE
+    # AND #$AA
+    # TAX
+    #
+    # The page goes on to say:
+    # On my 128, xx may be EE,EF,FE, OR FF.  These possibilities appear to depend 
+    # on three factors: the X register, PC, and the previous instruction 
+    # executed.  Bit 0 is ORed from x, and also from PCH.  As for XAA, on my 128 
+    # this opcode appears to work exactly as described in the list.
+    # On my 64, OAL produces all sorts of values for xx: 00,04,06,80, etc... A 
+    # rough scenario I worked out to explain this is here.  The constant value EE 
+    # disappears entirely.  Instead of ORing with EE, the accumulator is ORed 
+    # with certain bits of X and also ORed with certain bits of another 
+    # "register" (nature unknown, whether it be the data bus, or something else).  
+    # However, if OAL is preceded by certain other instructions like NOP, the 
+    # constant value EE reappears and the foregoing does not take place.
+    # 
+    #("lxab", "lxa immediate"),
+
+    # NOP tests
+    ("nopn", "nop"),  # $ea
+
+    # illegal op "NOP" tests
+    ("nopa", "nop absolute"),  # $0c
+    ("nopax", "nop absolute,x"),  # $1c
+    ("nopb", "nop immediate"),  # $80
+    ("nopz", "nop zeropage"),   # $04
+    ("nopzx", "nop zeropage,x"),  # $14
+
+    # ORA tests
     ("oraa", "ora absolute"),
     ("oraax", "ora absolute,x"),
     ("oraay", "ora absolute,y"),
@@ -409,17 +493,42 @@ binary_file_tests = [
     ("oraiy", "ora (indirect),y"),
     ("oraz", "ora zeropage"),
     ("orazx", "ora zeropage,x"),
+
+    # stack push/pull tests (PHA, PHP, PLA, PLP)
     ("phan", "pha"),
     ("phpn", "php"),
     ("plan", "pla"),
     ("plpn", "plp"),
-    ("rlaa", "rla absolute"),
-    ("rlaax", "rla absolute,x"),
-    ("rlaay", "rla absolute,y"),
-    ("rlaix", "rla (indirect,x)"),
-    ("rlaiy", "rla (indirect),y"),
-    ("rlaz", "rla zeropage"),
-    ("rlazx", "rla zeropage,x"),
+
+    # illegal op not yet supported in our emulator
+    # 
+    # RLA    ***
+    # RLA ROLs the contents of a memory location and then ANDs the result with 
+    # the accumulator.
+    # Supported modes:
+    # RLA abcd        ;2F cd ab    ;No. Cycles= 6
+    # RLA abcd,X      ;3F cd ab    ;            7
+    # RLA abcd,Y      ;3B cd ab    ;            7
+    # RLA ab          ;27 ab       ;            5
+    # RLA ab,X        ;37 ab       ;            6
+    # RLA (ab,X)      ;23 ab       ;            8
+    # RLA (ab),Y      ;33 ab       ;            8
+    # (Sub-instructions: AND, ROL)
+    # Here's an example of how you might write it in a program.
+    # RLA $FC,X       ;37 FC
+    # Here's the same code using equivalent instructions.
+    # ROL $FC,X
+    # AND $FC,X
+    # 
+    #("rlaa", "rla absolute"),
+    #("rlaax", "rla absolute,x"),
+    #("rlaay", "rla absolute,y"),
+    #("rlaix", "rla (indirect,x)"),
+    #("rlaiy", "rla (indirect),y"),
+    #("rlaz", "rla zeropage"),
+    #("rlazx", "rla zeropage,x"),
+
+    # rotate tests (ROL, ROR)
     ("rola", "rol absolute"),
     ("rolax", "rol absolute,x"),
     ("roln", "rol"),
@@ -430,6 +539,26 @@ binary_file_tests = [
     ("rorn", "ror"),
     ("rorz", "ror zeropage"),
     ("rorzx", "ror zeropage,x"),
+
+    # illegal op not yet supported in our emulator
+    # 
+    # RRA    ***
+    # RRA RORs the contents of a memory location and then ADCs the result with 
+    # the accumulator.
+    # Supported modes:
+    # RRA abcd        ;6F cd ab    ;No. Cycles= 6
+    # RRA abcd,X      ;7F cd ab    ;            7
+    # RRA abcd,Y      ;7B cd ab    ;            7
+    # RRA ab          ;67 ab       ;            5
+    # RRA ab,X        ;77 ab       ;            6
+    # RRA (ab,X)      ;63 ab       ;            8
+    # RRA (ab),Y      ;73 ab       ;            8
+    # (Sub-instructions: ADC, ROR)
+    # Example:
+    # RRA $030C       ;6F 0C 03
+    # Equivalent instructions:
+    # ROR $030C
+    # ADC $030C
     ("rraa", "rra absolute"),
     ("rraax", "rra absolute,x"),
     ("rraay", "rra absolute,y"),
@@ -437,21 +566,60 @@ binary_file_tests = [
     ("rraiy", "rra (indirect),y"),
     ("rraz", "rra zeropage"),
     ("rrazx", "rra zeropage,x"),
+
+    # return tests (RTI, RTS)
     ("rtin", "rti"),
     ("rtsn", "rts"),
+
+    # SBC tests
     ("sbca", "sbc absolute"),
     ("sbcax", "sbc absolute,x"),
     ("sbcay", "sbc absolute,y"),
     ("sbcb", "sbc immediate"),
-    ("sbcb(eb)", "sbcb(eb)"),
     ("sbcix", "sbc (indirect,x)"),
     ("sbciy", "sbc (indirect),y"),
     ("sbcz", "sbc zeropage"),
     ("sbczx", "sbc zeropage,x"),
-    ("sbxb", "sbx immediate"),
+
+    # illegal op SBC $EB (equivalent to $E9)
+    ("sbcb(eb)", "sbcb(eb)"),
+
+    # illegal op not yet supported in our emulator
+    # 
+    # Note:  illegal op names aren't standardized, not just different naming, but
+    # sometimes exchanged uses of the same names.  e.g., I've seen some flip
+    # their "SAX" and "AXS" labels
+    # 
+    # SAX    *** (aka SBX, aka AXS)
+    # SAX ANDs the contents of the A and X registers (leaving the contents of A 
+    # intact), subtracts an immediate value, and then stores the result in X.
+    # ... A few points might be made about the action of subtracting an immediate 
+    # value.  It actually works just like the CMP instruction, except that CMP 
+    # does not store the result of the subtraction it performs in any register.  
+    # This subtract operation is not affected by the state of the Carry flag, 
+    # though it does affect the Carry flag.  It does not affect the Overflow 
+    # flag.
+    # One supported mode:
+    # SAX #ab         ;CB ab       ;No. Cycles= 2
+    # Example:
+    # SAX #$5A        ;CB 5A
+    # Equivalent instructions:
+    # STA $02
+    # TXA
+    # AND $02
+    # SEC
+    # SBC #$5A
+    # TAX
+    # LDA $02
+    # Note: Memory location $02 would not be altered by the SAX opcode.
+    # 
+    #("sbxb", "sbx immediate"),
+
+    # set flag instruction tests
     ("secn", "sec"),
     ("sedn", "sed"),
     ("sein", "sei"),
+
     ("shaay", "sha absolute,y"),
     ("shaiy", "sha (indirect),y"),
     ("shsay", "shs absolute,y"),
@@ -496,10 +664,9 @@ binary_file_tests = [
 
 start_tests_at = 0 # start at the beginning
 # when debugging, start at the named test:
-start_tests_at = [a_tuple[0] for a_tuple in binary_file_tests].index('irq')
+start_tests_at = [a_tuple[0] for a_tuple in binary_file_tests].index('shaay')
 
 tests_to_run = binary_file_tests[start_tests_at:]
-
 
 
 # translate alphabet from mixed-case (mode) petscii to ascii
@@ -642,7 +809,6 @@ class TestWolfgangLorenzPrograms(unittest.TestCase):
             #if cpuState.cpucycles == 2149:
             #    if (1==1):
             #        pass
-
 
             # Capture petscii characters sent to screen print routine
             if cpuState.pc == 65490: # $FFD2
