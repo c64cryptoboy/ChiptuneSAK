8a195f4eaf055eedb296ec1ecef7a5bb757c9991
David Youd <cryptoboy@gmail.com>
2020-06-20 08:59:37 -0700

    added tests for constants freq stuff

--- test/baseTest.py
+++ test/baseTest.py
@@ -47,3 +47,15 @@ class BaseTestCase(unittest.TestCase):
         # Make a tone exactly 12 cents higher than C4
         f = 261.63 * 2**(1 / 100)
         self.assertEqual(ctsConstants.freq_to_midi_num(f), (ctsConstants.C4_MIDI_NUM, 12))
+
+        note_name = 'C0'
+        midi_note = note_name_to_pitch(note_name)
+        self.assertEqual(int(ctsConstants.midi_num_to_freq(midi_note) * 1000), 16351)
+        self.assertEqual(ctsConstants.midi_num_to_freq_arch(midi_note, arch='NTSC-C64'), 268)
+        self.assertEqual(ctsConstants.midi_num_to_freq_arch(midi_note, arch='PAL-C64'), 278)
+        self.assertEqual(ctsConstants.freq_arch_to_midi_num(268, arch='NTSC-C64')[0], midi_note)
+        self.assertEqual(ctsConstants.freq_arch_to_midi_num(278, arch='PAL-C64')[0], midi_note)
+
+
+if __name__ == '__main__':
+    unittest.main(failfast=False)
