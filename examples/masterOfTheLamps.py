ce2f9ff3b0e69da2cdc071715db629884c33771e
David Youd <cryptoboy@gmail.com>
2020-07-01 07:30:10 -0700

    motl subtune extraction wip

--- examples/masterOfTheLamps.py
+++ examples/masterOfTheLamps.py
@@ -1,20 +1,5 @@
-from chiptunesak.constants import project_to_absolute_path
-from chiptunesak.sid import SID
-from chiptunesak import midi
-
-sid_filename = project_to_absolute_path('tests/sid/Master_of_the_Lamps_PAL.sid')
-
-# TODO:
-# The rchirp song goes way too fast when converted to midi, probably need to specify
-# how many rows per quarter note or something.
+# Export subtunes from Master of the Lamps for processing in Sibelius.
 #
-# Sound comparison:
-# https://deepsid.chordian.net/?file=/MUSICIANS/L/Lieblich_Russell/Master_of_the_Lamps_PAL.sid
-#
-# Dump comparison:
-# ./siddump.exe Master_of_the_Lamps_PAL.sid -a5 -t
-
-# list of subtunes to extract
 # Notes:
 # - 3 genies * 7 pieces * (1 tunnel level + 1 music level) + final tunnel = 43 levels
 #       odd numbers are tunnels, even numbers are genies
@@ -22,30 +7,93 @@ sid_filename = project_to_absolute_path('tests/sid/Master_of_the_Lamps_PAL.sid')
 #       final tunnel: level 43
 # - Song duration (in seconds) = ceil(last frame in spreadsheet / 50.124542)
 #       ChiptuneSAK trims spreadsheets when song ends, but some subtunes endlessly repeat
-# - TODO: v2 in in subtune 5 (zero indexed) has a number of pitch bends, always up, and
-#       indiscriminantly applied.  I don't see the v2 pitch bends in either this tool or
-#       siddump.c.  It's an RSID, so there's a small chance that the code that pitch bends
-#       is setup by the init routine, but is not executed in the play routine?  Guess I should
-#       look at the interrupt handler at some point.
+# - TODO: I don't see the v2 pitch bends in either this tool or siddump.c.  It's an RSID,
+#       so there's a small chance that the code that pitch bends is setup by the init routine?
+#       Guess I should look at the interrupt handler to see if it does pitch bend handing
+#       outside of play calls.  If so, freq would stay at its set point (since it's the only
+#       code getting called), which is what I observe.
 # - The starting key signatures (below) don't match the PAL subtunes this parses, instead
 #       they should match the NTSC game footage (https://csdb.dk/release/?id=164839) that I
 #       captured for the YouTube video that I'm going to match this music up with.
+#
+# TODOs:
+# - output midi is pretty fast when LCD=2, and reasonable when LCD=1.  Need
+#   to get a handle on how to set parameters for each subtune so that they
+#   comes into Sibelius closer to target.
+
+from chiptunesak.constants import project_to_absolute_path
+from chiptunesak.sid import SID
+from chiptunesak import midi
+
+sid_filename = project_to_absolute_path('tests/sid/Master_of_the_Lamps_PAL.sid')
+
+# list of subtunes to extract
 to_extract = [
-    [10, "getting on carpet", 21, 'Ebm', 2, 4],  # GCD=2 (1st repeat frame 1024)
-    [7, "carpet liftoff", 9, 'C', 2, 4],  # GCD=2  # KEY?
-    [9, "fell off carpet", 9, 'C', 4, 4],  # GCD=2 # KEY?
-    [8, "finished level", 12, 'C', 2, 4],  # GCD=2
-    [11, "game won", 23, 'C', 4, 4],  # GCD=2
-    [0, "tunnel 1", 77, 'C', 4, 4],  # level 1, 15, 29  # GCD=2
-    [1, "tunnel 2", 86, 'C', 4, 4],  # level 3, 17, 31  # GCD=2
-    [2, "tunnel 3", 62, 'Gm', 4, 4],  # level 5, 19, 33  # GCD=1
-    [3, "tunnel 4", 103, 'Cm', 4, 4],  # level 7, 21, 35  # GCD=2
-    [4, "tunnel 5", 94, 'Cm', 4, 4],  # level 9, 23, 37  # GCD=2
-    [5, "tunnel 6", 84, 'Em', 4, 4],   # level 11, 25, 39  # GCD=2  # V2 pitch bends
-    [6, "tunnel 7", 62, 'Dm', 4, 4],  # level 13, 27, 41  # GCD=2
-    [12, "tunnel 8", 86, 'Cm', 4, 4],  # level 43  # GCD=2
+    # GCD=2
+    # repeats every 1024 frames
+    # nice appoggiatura auditory illusion on the step up to the 5th intervals in v2 when
+    #     combined with v1
+    [10, "getting on carpet", 21, 'Ebm', 2, 4],
+
+    # GCD=2, no key
+    # v2 sound effect, v1-v3 end with woosh
+    [7, "carpet liftoff", 9, 'C', 2, 4],
+
+    # GCD=2, no key
+    # V2 pitch bends
+    [9, "fell off carpet", 9, 'C', 4, 4],
+
+    # GCD=2
+    [8, "finished level", 12, 'C', 2, 4],
+
+    # GCD=2
+    [11, "game won", 23, 'C', 4, 4],
+
+    # level 1, 15, 29
+    # GCD=2
+    # V2 split between sound effects (down sweeps) or notes (in sections)
+    # V3 split between percussions (down sweeps) and notes (in sections)
+    [0, "tunnel 1", 77, 'C', 4, 4],
+
+    # level 3, 17, 31
+    # GCD=2
+    # V3 split between percussions and notes (in sections)
+    [1, "tunnel 2", 86, 'C', 4, 4],
+
+    # level 5, 19, 33
+    # GCD=1
+    # V2 some detuning and upward pitch bends
+    # V3 split between percussions and notes (in sections)
+    [2, "tunnel 3", 62, 'Gm', 4, 4],
+
+    # level 7, 21, 35
+    # GCD=2
+    # V2 some detuning and pitch bends (both up and down)
+    # V3 split between percussions and notes (in sections)
+    [3, "tunnel 4", 103, 'Cm', 4, 4],
+
+    # level 9, 23, 37
+    # GCD=2
+    # V2 some sound effects, detuning and pitch bends (both up and down)
+    # V3 split between percussions and notes (in sections)
+    [4, "tunnel 5", 94, 'Cm', 4, 4],
+
+    # level 11, 25, 39
+    # GCD=2
+    # V2 pitch bends
+    # V3 percussion (except for last note)
+    [5, "tunnel 6", 84, 'Em', 4, 4],
+
+    # level 13, 27, 41
+    # GCD=2
+    [6, "tunnel 7", 62, 'Dm', 4, 4],
+
+    # level 43
+    # GCD=2
+    # V3 splits between percussion and notes (in sections)
+    [12, "tunnel 8", 86, 'Cm', 4, 4],
 ]
-to_extract = [to_extract[10]]  # TODO: Remove this later
+# to_extract = [to_extract[10]]  # Debugging
 
 
 def find_tunings():
