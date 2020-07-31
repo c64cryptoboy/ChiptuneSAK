baef1a8176c0594773b2521dd9d5e98c71fb43b5
fizzymagic <orders@fizzymagic.net>
2020-07-30 19:55:52 -0700

    Updated Skyfox triplet example

--- examples/tripletExample.py
+++ examples/tripletExample.py
@@ -11,60 +11,34 @@ TODO:
 - Can now get the Skyfox data from ChiptuneSAK itself.  Rewrite lilypond export to use that data.
 """
 
-sid_filename = project_to_absolute_path('examples/sid/Skyfox.sid')
+file_name = 'Skyfox'
+output_folder = str(project_to_absolute_path('examples/data/triplets/'))
+input_folder = str(project_to_absolute_path('examples/sid/'))
+input_sid_file = os.path.join(input_folder, file_name + '.sid')
+output_mid_file = os.path.join(output_folder, file_name + '.mid')
+output_mod_mid_file = os.path.join(output_folder, file_name + '_mod.mid')
+output_ly_file = os.path.join(output_folder, file_name + '.ly')
+output_ly_file_mod = os.path.join(output_folder, file_name + '_mod.ly')
 
 sid = SID()
 sid.set_options(
-    sid_in_filename=sid_filename,
+    sid_in_filename=input_sid_file,
     vibrato_cents_margin=0,
     seconds=100,  # Skyfox SID playback continues to repeat, 100 secs is enough
     verbose=True
 )
 
-sid_dump = sid.capture()  # noqa: F841
-
-# Notes:
-# - between play calls, the voice gates are always on.  They breifly turn off and back on again
-#   inside of play calls.  This is detected/handled by ChiptunesSAK.
-# - Voice 2 does a portamento from F4 to B2 and back to F3 from frame (play call) 3792 to 3818,
-#   during which, freq only seems to be updated every other frame.  Cent deviations are so small
-#   that it looks like it was stored as individual notes, and not as a portamento effect.
-
-filename_no_ext = 'examples/data/Skyfox'
-
-csv_filename = '%s.csv' % filename_no_ext
-print("writing %s" % csv_filename)
-sid.to_csv_file(project_to_absolute_path(csv_filename))
-
-midi_filename = '%s.mid' % filename_no_ext
-print("writing %s" % midi_filename)
 rchirp_song = sid.to_rchirp()
 
 # CSV shows 24 plays calls per quarter note
 #   24 = 2^3 * 3, the factor of 3 is necessary for all the division-by-three rhythms
 play_calls_per_quarter = 24
-chirp_song = \
-    rchirp_song.to_chirp(milliframes_per_quarter=play_calls_per_quarter * 1000)
+chirp_song = rchirp_song.to_chirp(milliframes_per_quarter=play_calls_per_quarter * 1000)
 
 chirp_song.set_key_signature('G')
-chirp_song.set_time_signature(6, 4)
+chirp_song.set_time_signature(4, 4)
 
-chiptunesak.MIDI().to_file(
-    chirp_song, project_to_absolute_path(midi_filename))
-
-exit("early exit")
-
-output_folder = str(project_to_absolute_path('examples\\data\\triplets')) + '\\'
-input_folder = output_folder
-input_mid_file = input_folder + 'skyfox.mid'
-output_mid_file = output_folder + 'skyfox_mod.mid'
-output_ly_file = output_folder + 'skyfox.ly'
-output_ly_file_mod = output_folder + 'skyfox_mod.ly'
-
-chirp_song = chiptunesak.MIDI().to_chirp(input_mid_file)
-
-original_qpm = chirp_song.metadata.qpm
-original_ppq = chirp_song.metadata.ppq
+chiptunesak.MIDI().to_file(chirp_song, project_to_absolute_path(output_mid_file))
 
 # First thing, we rename the song
 chirp_song.metadata.name = "SkyFox - Main Theme"
@@ -74,16 +48,12 @@ chirp_song.metadata.composer = "Douglas Fulton"
 for t, name in zip(chirp_song.tracks, ['Square1', 'Square2', 'Square3']):
     t.name = name
 
-chirp_song.scale_ticks(6.25)
-chirp_song.metadata.ppq = 960
-chirp_song.set_qpm(original_qpm * 1.25)
-chirp_song.set_time_signature(4, 4)
-chirp_song.set_key_signature('G')
-
 chirp_song.quantize(80, 80)
 chirp_song.remove_polyphony()
 
 mchirp_song = chirp_song.to_mchirp()
+for track in mchirp_song.tracks:
+    track.measures.pop()
 chiptunesak.Lilypond().to_file(mchirp_song, output_ly_file, format='song')
 
 # Change directory to the data directory so we don't fill the source directory with intermediate files.
