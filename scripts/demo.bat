419c2424a7964c98406594b34db15cd4728bdf5e
David Youd <cryptoboy@gmail.com>
2020-08-13 17:20:32 -0700

    set ntsc speed on one goattracker call and all c128 calls

--- scripts/demo.bat
+++ scripts/demo.bat
@@ -5,7 +5,7 @@ rem path to stereo GT
 set gtpath2=C:\Users\crypt\Desktop\sound\SIDwork\GoatTracker_2.76_Stereo\trunk\win32\gt2stereo.exe
 rem path to Vice C128
 set vicepath=C:\Users\crypt\Desktop\retro8-bit\c64\WinVICE-3.1-x86\x128.exe
- 
+
 rem Demo 1: Mercantile
 python ..\examples\mercantile.py
 echo Hit any key to load PDF
@@ -13,7 +13,7 @@ pause
 start /wait ..\examples\data\mercantile\mercantile.pdf
 echo Hit any key to load GoatTracker song
 pause
-start /wait %gtpath% ..\examples\data\mercantile\mercantile.sng
+start /wait %gtpath% -N ..\examples\data\mercantile\mercantile.sng
 
 rem Demo 2: LeChuck
 python ..\examples\lechuck.py
@@ -36,13 +36,13 @@ start /wait ..\examples\data\triplets\skyfox_mod.pdf
 rem Demo 4: C128
 echo Hit any key to load buggy BWV_784
 pause
-start /wait %vicepath% BWV_784_buggy.prg
+start /wait %vicepath% -ntsc -basicload BWV_784_buggy.prg
 python ..examples/c128_2_Voice_From_Manual.py
 echo Hit any key to load Vice C128 program
 pause
-start /wait %vicepath% ..\examples\data\C128\BWV_784.prg
+start /wait %vicepath% -ntsc -basicload ..\examples\data\C128\BWV_784.prg
 python ..examples/c128_3_Voice_Example.py
 echo Hit any key to load Vice C128 program
 pause
-start /wait %vicepath% ..\examples\data\C128\BWV_799.prg
+start /wait %vicepath% -ntsc -basicload ..\examples\data\C128\BWV_799.prg
 
