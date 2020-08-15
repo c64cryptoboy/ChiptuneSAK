8598e3231690698e7cb8c8fcd956985ad6e9f533
David Youd <cryptoboy@gmail.com>
2020-08-15 07:43:05 -0700

    cleans up of path, ignores, and yofa data

--- sandbox/sidImportDevTester.py
+++ sandbox/sidImportDevTester.py
@@ -4,23 +4,24 @@
 import chiptunesak
 from chiptunesak.constants import project_to_absolute_path
 
+dest = 'examples/temp/'
 
 def create_output_files():
     params = [
         # has the notes that sid2midi misses
-        (project_to_absolute_path('examples/sid/Pool_of_Radiance.sid'), 0, 'examples/por', 12, 120),
+        (project_to_absolute_path('examples/sid/Pool_of_Radiance.sid'), 0, dest + 'por', 12, 120),
 
-        (project_to_absolute_path('examples/sid/Skyfox.sid'), 0, 'examples/skyfox', 24, 100),
+        (project_to_absolute_path('examples/sid/Skyfox.sid'), 0, dest + 'skyfox', 24, 100),
 
         # contains digi, which is ignored
-        (project_to_absolute_path('examples/sid/Great_Giana_Sisters.sid'), 0, 'examples/ggs', 24, 30),
+        (project_to_absolute_path('examples/sid/Great_Giana_Sisters.sid'), 0, dest + 'ggs', 24, 30),
 
         # assert_gate_on_new_note: True includes arpeggios, and False includes just the chord root
-        (project_to_absolute_path('examples/sid/Butcher_Hill.sid'), 1, 'examples/bh', 12, 175),
+        (project_to_absolute_path('examples/sid/Butcher_Hill.sid'), 1, dest + 'bh', 12, 175),
 
-        (project_to_absolute_path('examples/sid/Nitro.sid'), 1, 'examples/nitro', 12, 15),
+        (project_to_absolute_path('examples/sid/Nitro.sid'), 1, dest + 'nitro', 12, 15),
 
-        (project_to_absolute_path('examples/sid/HelloWorld.sid'), 1, 'examples/hw', 12, 15),
+        (project_to_absolute_path('examples/sid/HelloWorld.sid'), 1, dest + 'hw', 12, 15),
 
     ]
     params = params[5]  # select one
