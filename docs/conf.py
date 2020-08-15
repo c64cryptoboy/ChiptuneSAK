e508ed8c63d01328e8a1182a77bd095cdb83db23
David Youd <cryptoboy@gmail.com>
2020-08-15 14:16:29 -0700

    waving the readthedocs chicken...

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,8 @@ release = constants.CHIPTUNESAK_RELEASE
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
